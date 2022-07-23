1938f6a43ef7f35e3248cd614ba73710f072ec40
shveykinvlad <shveykinvlad@gmail.com>
2022-07-23 16:48:43 +0200

    Refactoring

--- src/app/constants/api-url.ts
+++ src/app/constants/api-url.ts
@@ -2,13 +2,15 @@ import { environment } from 'src/environments/environment';
 
 export class ApiUrl {
 
+    public static readonly id: string = '/${id}'
+
     public static readonly users: string = environment.apiUrl + '/users'
-    public static readonly confirmation: string = ApiUrl.users + '/confirmation';
-    public static readonly password: string = ApiUrl.users + '/password'
-    public static readonly authentication: string = ApiUrl.users + '/authentication';
-    public static readonly refreshAccessToken: string = ApiUrl.users + '/access';
+    public static readonly confirmation: string = this.users + '/confirmation';
+    public static readonly password: string = this.users + '/password'
+    public static readonly authentication: string = this.users + '/authentication';
+    public static readonly refreshAccessToken: string = this.users + '/access';
 
     public static readonly notes: string = environment.apiUrl + '/notes';
-    public static readonly search: string = ApiUrl.notes + '/search';
-    public static readonly note: string = ApiUrl.notes + '/${id}';
+    public static readonly search: string = this.notes + '/search';
+    public static readonly note: string = this.notes + this.id;
 }
