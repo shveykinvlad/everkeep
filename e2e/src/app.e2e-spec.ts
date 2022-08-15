c1eb349fed7e0c71ad6e2dba6d3a2970e7636045
shveykinvlad <shveykinvlad@gmail.com>
2022-08-15 19:49:36 +0200

    Rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('everkeep-ui app is running!');
+    expect(page.getTitleText()).toEqual('everkeep app is running!');
   });
 
   afterEach(async () => {
